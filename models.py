c20fe852cf67dd4ce6ffc4adda2df9c8b0d45480
Ayman Abdi Mohamed <9100aymanabdi@gmail.com>
2025-11-27 12:47:42 +0300

    initial migration

--- models.py
+++ models.py
@@ -1,11 +1,13 @@
-from sqlalchemy.ext.declarative import declarative_base # for defining the base class
-from sqlalchemy import Column, Interger, String, DateTime # for defining columns and data types
+from sqlalchemy.ext.declarative import declarative_base  # for defining the base class
+from sqlalchemy import Column, Integer, String  # for defining columns and data types
 
 Base = declarative_base()
+
 
 # must have at least one columns
 # the table must be provided via the attribute __tablename__
 class Menu(Base):
-    __table__name = 'menus' # class attribute providing the table name
-    
-    id = Column(Interger, primary_key-True) # primary key column
+    __tablename__ = 'menus'  # class attribute providing the table name
+
+    id = Column(Integer, primary_key=True)  # primary key column
+    name = Column(String())  # name column
